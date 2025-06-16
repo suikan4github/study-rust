44363316753732a43e597abc1f32ec5fba061276
Seiichi Horie <26223147+suikan4github@users.noreply.github.com>
2025-06-16 22:24:07 +0900

    new_arrivals.jsonに対応

--- p0190-reading-json/src/main.rs
+++ p0190-reading-json/src/main.rs
@@ -21,6 +21,24 @@ struct Series {
     title: String,
     episodes: Vec<Episode>,
 }
+
+// コーナー構造体を定義する
+#[derive(Serialize, Deserialize, Debug)]
+//#[allow(non_snake_case)]
+struct Corner {
+    title: String,
+    corner_name: String,
+    series_site_id: String,
+    corner_site_id: String,
+}
+
+// 新着情報構造体を定義する
+#[derive(Serialize, Deserialize, Debug)]
+//#[allow(non_snake_case)]
+struct NewInfo {
+    corners: Vec<Corner>,
+}
+
 fn main() {
     // series.jsonを読み込む
     let file_path = "series.json";
@@ -35,6 +53,22 @@ fn main() {
         println!(
             r#"{{"program_title": "{}","stream_url": "{}"}},"#,
             e.program_title, e.stream_url
+        );
+    }
+
+    // new_arrivals.jsonを読み込む
+    let new_arrivals_path = "new_arrivals.json";
+    let new_arrivals_content = fs::read_to_string(new_arrivals_path).expect("Unable to read file");
+
+    // JSONをデシリアライズする
+    let new_info: NewInfo =
+        serde_json::from_str(&new_arrivals_content).expect("JSON was not well-formatted");
+
+    // 各コーナーのtitle, corner_name,series_site_id, corner_site_id, を表示する
+    for corner in new_info.corners {
+        println!(
+            r#"{{"title": "{}","corner_name": "{}","series_site_id": "{}","corner_site_id": "{}"}},"#,
+            corner.title, corner.corner_name, corner.series_site_id, corner.corner_site_id
         );
     }
 }
